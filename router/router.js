c6cc2564971101be212b12502e055973e50f1e09
70001511952020 <heriyanto.sitorus@brilife.co.id>
2021-06-25 13:00:51 +0700

    modif router API

--- router/router.js
+++ router/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const user = require('../controller/User')
+const user = require('../controller/user')
 
 // API USER
 router.get('/getuser', user.getAllData);
-
+router.post('/addOneUser', user.addOneData);
+router.post('/editOneUser', user.editOneData);
+router.post('/deleteOneUser', user.deleteOneData);
 
 module.exports = router;
