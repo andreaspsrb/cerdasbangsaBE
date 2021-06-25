f1673989f4e2086c6153e15144f19fcc7b3bb451
andreaspsrb <pasaribuandreas622@gmail.com>
2021-06-25 15:29:15 +0700

    get API KELAS

---
+++ controller/kelas.js
@@ -0,0 +1,20 @@
+'use strict';
+let response = require('../res/res');
+let connection = require('../config/connect');
+
+let getAllData = (req, res) => {
+    
+    let qry = 'SELECT * FROM Kelas';
+    connection.query(qry, (error, result) =>{
+        if (error){
+            console.log(error);
+        }else{
+            response.ok(result, res)
+            console.log(result);
+        }
+    })
+}
+
+module.exports = {
+    getAllData
+}
--- router/router.js
+++ router/router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const user = require('../controller/user')
-
+const kelas = require('../controller/kelas')
 // API USER
 router.get('/getuser', user.getAllData);
 router.post('/addOneUser', user.addOneData);
 router.post('/editOneUser', user.editOneData);
 router.post('/deleteOneUser', user.deleteOneData);
+
+//API KELAS
+router.get('/getkelas',kelas.getAllData );
 
 module.exports = router;
