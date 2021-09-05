bc27729b2345909cb840d75e475aa7d8c9cfdad3
70001511952020 <70001511952020@brilife.co.id>
2021-09-05 23:55:49 +0700

    fix query

--- controller/user.js
+++ controller/user.js
@@ -105,11 +105,15 @@ let addOneData = (req, res) => {
 
      let qry =`SELECT u.username, u.password, u.role, s.* FROM User u 
      INNER JOIN siswa s ON s.nis = u.username
-    WHERE .username = '${username}' AND password = '${password}'`
+    WHERE username = '${username}' AND password = '${password}'`
      
     //  `SELECT username, password, role FROM User WHERE username = '${username}' AND password = '${password}'`
  
      connection.query(qry, (error, result) => {
+    //    cek isi data 
+        console.log(result); 
+        // cek query
+        console.log(qry);
       if (result.length > 0) {
 
          let data = {
@@ -119,13 +123,24 @@ let addOneData = (req, res) => {
          };
          res.json(data);
          res.end();
-      } else {
-         let error = {
+      } else if (result.length == 0) {
+         let err = {
              'status': 500,
              'message' :'error',
-             'values': 'tidak berhasil login'
+             'values': 'data tidak ada'
+
          };
-         res.json(error);
+         res.json(err);
+         res.end();
+        }
+         else  {
+            let err = {
+                'status': 500,
+                'message' :'error',
+                'values': 'tidak berhasil login',
+                'detail': error
+            };
+         res.json(err);
          res.end();
       }
   })
